c0ffdaa50670d6f4d75ec195b6739de45e597f9a
Evan Krambuhl <thekrambuhl@gmail.com>
2016-03-15 00:06:29 -0700

    children scope fix

--- test/Children.test.js
+++ test/Children.test.js
@@ -4,19 +4,19 @@ var Children = require('../dist').Children;
 var Heading = require('./fixtures/templates.json').Heading;
 var Link = require('./fixtures/templates.json').Link;
 
-test('<Children /> :: with @children', function(t) {
+test('<Children /> :: with children', function(t) {
   t.plan(2);
 
-  var res = Children({ type: Heading }, { '@children': Link });
+  var res = Children({ type: Heading, children: Link });
 
   t.equal(res.type, Link.type);
   t.equal(res.name, Link.name);
 });
 
-test('<Children /> :: without @children', function(t) {
+test('<Children /> :: without children', function(t) {
   t.plan(1);
 
-  var res = Children({ type: Heading }, { '@children': undefined });
+  var res = Children({ type: Heading });
 
   t.equal(res === undefined, true);
 });
